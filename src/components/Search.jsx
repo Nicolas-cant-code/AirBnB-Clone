3a3590f3522948ffb39d525a87f6ada831de3c79
Nicolas Webb (STADIO - Head Office) <nicw@stadio.ac.za>
2025-04-24 14:46:57 +0200

    Up until the cards

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,14 +1,39 @@
 import React, { useState } from "react";
 import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { DateRangePicker } from "react-date-range";
+import PeopleIcon from "@mui/icons-material/People";
+import { Button } from "@mui/material";
 import "./Search.css";
 
 const Search = () => {
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
 
+  const seletionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: "selection",
+  };
+
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+  };
+
   return (
-    <div>
-      <h1>Search</h1>
+    <div className="search">
+      <DateRangePicker onChange={handleSelect} ranges={[seletionRange]} />
+      <h2>
+        Number of guests <PeopleIcon />
+      </h2>
+      <input
+        type="number"
+        min={0}
+        defaultValue={2}
+        placeholder="Enter number of guests"
+      />
+      <Button>Search AirBnB</Button>
     </div>
   );
 };
